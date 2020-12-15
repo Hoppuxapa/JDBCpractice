b876dc69cacb84ecda59b17212c6e634a51ae48a
Ruslan <hoppuxapa@gmail.com>
2020-12-15 18:08:02 +0200

    Remove files

--- src/main/java/com/kravchuk/jdbc/config/DataAccessObject.java
+++ src/main/java/com/kravchuk/jdbc/config/DataAccessObject.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public abstract class DataAccessObject <T extends DataTransferObject> {
     public abstract void createOrUpdate(T object);
-   /* public abstract void update(T object);*/
     public abstract T getById(int id);
     public abstract List<T> getAll();
     public abstract void remove(int id);
